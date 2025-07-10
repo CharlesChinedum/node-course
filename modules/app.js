d7fe07e25f7039d529febef56ae6533d85259eda
charleschinedum <charleschinedum2@gmail.com>
2025-07-11 00:53:33 +0100

    enhance memory logging: add total and free memory calculations

--- modules/app.js
+++ modules/app.js
@@ -1,5 +1,12 @@
 const path = require("path");
+const os = require("os");
 
 const pathName = path.parse(__filename);
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
 
-console.log(pathName);
+// console.log(pathName);
+console.log(`Total Memory: ${totalMemory / 1024 / 1024 / 1024} GB`);
+console.log(`Free memory: ${freeMemory / 1024 / 1024 / 1024} GB`);
+console.log(`Free memory: ${freeMemory / 1024 / 1024} MB`);
+console.log(`Free memory: ${freeMemory}`);
