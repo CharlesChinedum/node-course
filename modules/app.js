5e833fd55987313534fbabd2d0811c277c50ff22
charleschinedum <charleschinedum2@gmail.com>
2025-07-11 01:19:08 +0100

    enhance app.js: add OS information and uptime logging

--- modules/app.js
+++ modules/app.js
@@ -1,12 +1,24 @@
 const path = require("path");
 const os = require("os");
-
 const pathName = path.parse(__filename);
 const totalMemory = os.totalmem();
 const freeMemory = os.freemem();
+const release = os.release();
+const osType = os.type();
+const platform = os.platform();
+const userInfo = os.userInfo();
+const uptime = os.uptime();
 
 // console.log(pathName);
 console.log(`Total Memory: ${totalMemory / 1024 / 1024 / 1024} GB`);
 console.log(`Free memory: ${freeMemory / 1024 / 1024 / 1024} GB`);
 console.log(`Free memory: ${freeMemory / 1024 / 1024} MB`);
 console.log(`Free memory: ${freeMemory}`);
+console.log(`OS: ${osType}`);
+console.log(`Release: ${release}`);
+console.log(`Platform: ${platform}`);
+console.log(`User Info: ${JSON.stringify(userInfo)}`);
+console.log(`Uptime: ${uptime} seconds`);
+console.log(`Uptime: ${uptime / 60} minutes`);
+console.log(`Uptime: ${uptime / 60 / 60} hours`);
+console.log(`Uptime: ${uptime / 60 / 60 / 24} days`);
